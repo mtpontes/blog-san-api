469c090beb5f271c902b551275482f51bc80921d
Mateus <mateuspsdd@gmail.com>
2024-02-08 06:56:54 -0300

    Start project

---
+++ src/main/java/br/com/blogsanapi/BlogSanApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.blogsanapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogSanApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogSanApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/blogsanapi/BlogSanApplicatio.java
@@ -0,0 +1,13 @@
+package br.com.blogsanapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogSanApplicatio {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
