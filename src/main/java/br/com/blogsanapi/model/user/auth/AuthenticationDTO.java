889e609df05e7ca16bb34799a0b48391fd9d7d25
Mateus <mateuspsdd@gmail.com>
2024-02-10 13:07:06 -0300

    ...

--- src/main/java/br/com/blogsanapi/model/user/auth/AuthenticationDTO.java
+++ src/main/java/br/com/blogsanapi/model/user/auth/AuthenticationDTO.java
@@ -1,4 +1,10 @@
 package br.com.blogsanapi.model.user.auth;
 
-public record AuthenticationDTO(String login, String password) {
+import jakarta.validation.constraints.NotBlank;
+
+public record AuthenticationDTO(
+		@NotBlank
+		String login, 
+		@NotBlank
+		String password) {
 }
