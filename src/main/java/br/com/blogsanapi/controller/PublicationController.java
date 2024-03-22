2bd15d295c89970a869de0f20785711bf48686ee
Mateus Pontes <mateuspsdd@gmail.com>
2024-03-22 18:19:10 -0300

    Move all methods from CommentController to PublicationController

--- src/main/java/br/com/blogsanapi/controller/PublicationController.java
+++ src/main/java/br/com/blogsanapi/controller/PublicationController.java
@@ -17,23 +17,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import br.com.blogsanapi.model.comment.request.CommentRepliRequestDTO;
+import br.com.blogsanapi.model.comment.request.CommentRequestDTO;
+import br.com.blogsanapi.model.comment.request.CommentUpdateDTO;
+import br.com.blogsanapi.model.comment.response.CommentResponseDTO;
 import br.com.blogsanapi.model.publication.request.PublicationRequestDTO;
 import br.com.blogsanapi.model.publication.request.PublicationUpdateRequestDTO;
 import br.com.blogsanapi.model.publication.response.PublicationResponseDTO;
 import br.com.blogsanapi.model.publication.response.PublicationResponseWithCommentsDTO;
+import br.com.blogsanapi.service.CommentService;
 import br.com.blogsanapi.service.PublicationService;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 
 @RestController
-@RequestMapping("/blog/publications")
+@RequestMapping("/blog")
 @SecurityRequirement(name = "bearer-key")
 public class PublicationController {
+	
 	@Autowired
 	private PublicationService publicationService;
+	@Autowired
+	private CommentService commentService;
 	
-	@PostMapping("/create")
+	
+	@PostMapping("/publications/create")
 	@Transactional
 	protected ResponseEntity<PublicationResponseDTO> createPublication(@RequestBody @Valid PublicationRequestDTO dto, UriComponentsBuilder uriBuilder){
 		PublicationResponseDTO dtoResponse = publicationService.createPublication(dto);
@@ -43,33 +52,77 @@ public class PublicationController {
 		return ResponseEntity.created(uri).body(dtoResponse);
 	}
 	
-	@GetMapping("/{id}")
-	protected ResponseEntity<PublicationResponseWithCommentsDTO> test(@PageableDefault(size = 5) Pageable pageable, @PathVariable Long id) {
+	@GetMapping("/publications/{id}")
+	protected ResponseEntity<PublicationResponseWithCommentsDTO> getPublication(@PageableDefault(size = 5) Pageable pageable, @PathVariable Long id) {
 		return ResponseEntity.ok(publicationService.getAPublicationWhithComments(pageable, id));
 	}
-	@GetMapping("/all")
+	@GetMapping("/publications")
 	protected ResponseEntity<Page<PublicationResponseDTO>> getPublicationsByDate(@PageableDefault(size = 10) Pageable pageable) {
 		return ResponseEntity.ok(publicationService.getAllPublications(pageable));
 	}
-	@GetMapping("/by-date/{date}")
+	@GetMapping("/publications/by-date/{date}")
 	protected ResponseEntity<Page<PublicationResponseDTO>> getPublicationsByDate(@PageableDefault(size = 10) Pageable pageable, @PathVariable @Valid LocalDate date) {
 		return ResponseEntity.ok(publicationService.getAllPublicationsByDate(pageable, date));
 	}
-	@GetMapping("/by-user/{id}")
+	@GetMapping("/publications/by-user/{id}")
 	protected ResponseEntity<Page<PublicationResponseDTO>> getAllPublicationsByUser(@PageableDefault(size = 10) Pageable pageable, @PathVariable Long id) {
 		return ResponseEntity.ok(publicationService.getAllPublicationsByUser(pageable, id));
 	}
 	
-	@PutMapping("/update")
+	@PutMapping("/publications/update")
 	@Transactional
 	protected ResponseEntity<PublicationResponseDTO> updatePublication(@RequestBody @Valid PublicationUpdateRequestDTO dto) {
 		return ResponseEntity.ok(publicationService.updatePublication(dto));
 	}
 	
-	@DeleteMapping("/delete/{id}")
+	@DeleteMapping("/publications/delete/{id}")
 	@Transactional
 	protected ResponseEntity<?> deletePublication(@PathVariable Long id) {
 		publicationService.deletePublication(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	
+	@PostMapping("/comments/create")
+	public ResponseEntity<CommentResponseDTO> createComment(@RequestBody @Valid CommentRequestDTO dto, UriComponentsBuilder uriBuilder) {
+		CommentResponseDTO commentResponse = commentService.createComment(dto);
+		
+		var uri = uriBuilder.path("/blog/comments/{id}").buildAndExpand(dto).toUri();
+		
+		return ResponseEntity.created(uri).body(commentResponse);
+	}
+	@PostMapping("/comments/create/reply")
+	public ResponseEntity<CommentResponseDTO> replyComment(@RequestBody @Valid CommentRepliRequestDTO dto, UriComponentsBuilder uriBuilder) {
+		CommentResponseDTO commentResponse = commentService.replyComment(dto);
+		
+		var uri = uriBuilder.path("/blog/comments/{id}").buildAndExpand(dto).toUri();
+		
+		return ResponseEntity.created(uri).body(commentResponse);
+	}
+	
+	@GetMapping("/comments/replies/{id}")
+	public ResponseEntity<Page<CommentResponseDTO>> getAllRepliesByComment(@PageableDefault(size = 5) Pageable pageable, @PathVariable Long id) {
+		return ResponseEntity.ok(commentService.getRepliesByComment(pageable, id));
+	}
+	
+	@GetMapping("/comments/by-user/{id}")
+	public ResponseEntity<Page<CommentResponseDTO>> getAllCommentsByUser(@PageableDefault(size = 10) Pageable pageable, @PathVariable Long id) {
+		return ResponseEntity.ok(commentService.getCommentsByUser(pageable, id));
+	}
+	@GetMapping("/comments/by-publication/{id}")
+	public ResponseEntity<Page<CommentResponseDTO>> getCommentsByPublication(@PageableDefault(size = 10) Pageable pageable, @PathVariable Long id) {
+		return ResponseEntity.ok(commentService.getCommentsByPublicationId(pageable, id));
+	}
+	
+	@PutMapping("/comments/update/{id}")
+	public ResponseEntity<CommentResponseDTO> updateComment(@RequestBody @Valid CommentUpdateDTO dto, @PathVariable Long id) {
+		CommentResponseDTO commentResponse = commentService.updateComment(dto, id);
+		return ResponseEntity.ok(commentResponse);
+	}
+	
+	@DeleteMapping("/comments/delete/{id}")
+	public ResponseEntity<?> deleteComment(@PathVariable Long id) {
+		commentService.deleteComment(id);
 		return ResponseEntity.noContent().build();
 	}
 }
