ae41ebcfe3557de9827152d19750922f35622af9
Mateus Pontes <mateuspsdd@gmail.com>
2024-05-22 20:19:39 -0300

    Refactor `PublicationRepository`
    - Removed `findAllByDate` method.

--- src/main/java/br/com/blogsanapi/repository/PublicationRepository.java
+++ src/main/java/br/com/blogsanapi/repository/PublicationRepository.java
@@ -20,9 +20,6 @@ public interface PublicationRepository extends JpaRepository<Publication, Long>{
 			""")
 	Page<Publication> findAllByParams(Pageable pageable, LocalDate date, Long userId);
 	
-	@Query("SELECT p FROM Publication p WHERE DATE(p.date) = :date")
-	Page<Publication> findAllByDate(Pageable pageable, LocalDate date);
-
 	void deleteByUserIdAndId(Long userId, Long publiId);
 
 	Page<Publication> findAllByUserId(Pageable pageable, Long id);
