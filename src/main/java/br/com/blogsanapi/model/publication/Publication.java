eddaadd5f4bfea77ca7e6c3d3aa629ad29b30a2a
Mateus Pontes <mateuspsdd@gmail.com>
2024-05-27 16:22:24 -0300

    Update `Publication`
    - Added @Builder annotation

--- src/main/java/br/com/blogsanapi/model/publication/Publication.java
+++ src/main/java/br/com/blogsanapi/model/publication/Publication.java
@@ -17,6 +17,7 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -26,6 +27,7 @@ import lombok.Setter;
 @Table(name = "publications")
 @Getter
 @NoArgsConstructor
+@Builder
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
 public class Publication {
