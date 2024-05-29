7467175540f249060af21428d15263580270214f
Mateus Pontes <mateuspsdd@gmail.com>
2024-05-29 18:28:16 -0300

    Update
    - Default ADMIN user creation logic moved from `BlogSanApiApplication` to `SecurityConfigurations`

--- src/main/java/br/com/blogsanapi/BlogSanApiApplication.java
+++ src/main/java/br/com/blogsanapi/BlogSanApiApplication.java
@@ -1,36 +1,18 @@
 package br.com.blogsanapi;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-import br.com.blogsanapi.model.user.User;
-import br.com.blogsanapi.model.user.UserRole;
 import br.com.blogsanapi.repository.UserRepository;
 
 @SpringBootApplication
-public class BlogSanApiApplication implements CommandLineRunner {
+public class BlogSanApiApplication {
 	
 	@Autowired
 	private UserRepository repository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(BlogSanApiApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		if(!repository.existsByLogin("root")) {
-			User userAdmin = User.builder()
-				.name("admin")
-				.login("root")
-				.password(new BCryptPasswordEncoder().encode("root"))
-				.role(UserRole.ADMIN)
-				.build();
-
-			repository.save(userAdmin);
-		}
 	}
 }
