d479871b2a2d2fb95e00a8bf6c3324f3621d5899
Mateus Pontes <mateuspsdd@gmail.com>
2024-05-30 10:41:08 -0300

    Update `User` roles

--- src/main/java/br/com/blogsanapi/model/user/User.java
+++ src/main/java/br/com/blogsanapi/model/user/User.java
@@ -60,8 +60,8 @@ public class User implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        if(this.role == UserRole.ADMIN) return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_USER"));
-        else return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+        if(this.role == UserRole.ADMIN) return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_CLIENT"));
+        else return List.of(new SimpleGrantedAuthority("ROLE_CLIENT"));
     }
 
     @Override
