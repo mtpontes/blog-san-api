0086d769a5f3b88c2c54cfd4d478d32f85ffc6a8
Mateus Pontes <mateuspsdd@gmail.com>
2024-05-29 22:27:09 -0300

    ...

--- src/main/java/br/com/blogsanapi/BlogSanApiApplication.java
+++ src/main/java/br/com/blogsanapi/BlogSanApiApplication.java
@@ -1,17 +1,10 @@
 package br.com.blogsanapi;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import br.com.blogsanapi.repository.UserRepository;
-
 @SpringBootApplication
 public class BlogSanApiApplication {
-	
-	@Autowired
-	private UserRepository repository;
-	
 	public static void main(String[] args) {
 		SpringApplication.run(BlogSanApiApplication.class, args);
 	}
