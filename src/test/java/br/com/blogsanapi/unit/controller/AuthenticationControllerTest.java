efcedb7b6ee74ca116810be0988a2f3f5f8d59e1
Mateus Pontes <mateuspsdd@gmail.com>
2024-05-29 22:28:59 -0300

    Add `AuthenticationControllerTest` and `TokenUtils`
    - Test classes

---
+++ src/test/java/br/com/blogsanapi/unit/controller/AuthenticationControllerTest.java
@@ -0,0 +1,144 @@
+package br.com.blogsanapi.unit.controller;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import br.com.blogsanapi.model.user.User;
+import br.com.blogsanapi.model.user.UserRole;
+import br.com.blogsanapi.model.user.auth.AuthenticationDTO;
+import br.com.blogsanapi.model.user.auth.LoginResponseDTO;
+import br.com.blogsanapi.model.user.auth.RegisterDTO;
+import br.com.blogsanapi.repository.UserRepository;
+import br.com.blogsanapi.utils.TokenUtils;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureJsonTesters
+@ActiveProfiles(profiles = "test")
+public class AuthenticationControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private JacksonTester<AuthenticationDTO> authenticationDTOJson;
+    @Autowired
+    private JacksonTester<RegisterDTO> registerDTOJson;
+    @Autowired
+    private JacksonTester<LoginResponseDTO> loginResponseDTOJson;
+
+    @MockBean
+    private AuthenticationManager authenticationManager;
+    @MockBean
+    private Authentication authentication;
+    @MockBean
+    private UserRepository repository;
+
+    // Mocked User
+    private final User userMock = User.builder()
+            .id(1L)
+            .login("root")
+            .password(new BCryptPasswordEncoder().encode("root"))
+            .role(UserRole.ADMIN)
+            .name("Name Default")
+            .email("default@email.com")
+            .build();
+
+    @Test
+    @DisplayName("Login Test - Should return valid token")
+    void loginTest() throws Exception {
+        // arrange
+        var requestBody = new AuthenticationDTO("test", "test");
+        when(authenticationManager.authenticate(any())).thenReturn(authentication);
+        when(authentication.getPrincipal()).thenReturn(userMock);
+
+        // act
+        var result = mvc.perform(
+                post("/auth/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(authenticationDTOJson.write(requestBody).getJson()))
+                .andReturn().getResponse();
+        var responseBody = loginResponseDTOJson.parseObject(result.getContentAsString());
+
+        // assert
+        Assertions.assertEquals(HttpStatus.OK.value(), result.getStatus(), "The status should be 200 OK");
+        Assertions.assertNotNull(responseBody.token(), "The token should not be null");
+        Assertions.assertFalse(responseBody.token().isBlank(), "The token should not be blank");
+        Assertions.assertTrue(TokenUtils.isValidTokenFormat(responseBody.token()), "The token format should be valid");
+    }
+
+    @Test
+    @DisplayName("Register Test - Should return OK status")
+    void registerTest() throws Exception {
+        // arrange
+        var requestBody = new RegisterDTO("newUser", "test", "Name Test", "email@test.com");
+        when(repository.save(any())).thenReturn(userMock);
+
+        // act
+        var result = mvc.perform(
+                post("/auth/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(registerDTOJson.write(requestBody).getJson()))
+                .andReturn().getResponse();
+
+        // assert
+        Assertions.assertEquals(HttpStatus.OK.value(), result.getStatus(), "The status should be 200 OK");
+    }
+
+    @Test
+    @DisplayName("Admin Register Test 01 - Should return OK status")
+    @WithMockUser(roles = "ADMIN")
+    void adminRegisterTest01() throws Exception {
+        // arrange
+        var requestBody = new RegisterDTO("newUser", "test", "Name Test", "email@test.com");
+        when(repository.save(any())).thenReturn(userMock);
+
+        // act
+        var result = mvc.perform(
+                post("/auth/admin/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(registerDTOJson.write(requestBody).getJson()))
+                .andReturn().getResponse();
+
+        // assert
+        Assertions.assertEquals(HttpStatus.OK.value(), result.getStatus(), "The status should be 200 OK");
+    }
+
+    @Test
+    @DisplayName("Admin Register Test 02 - Should return FORBIDDEN status")
+    @WithMockUser(roles = "CLIENT")
+    void adminRegisterTest02() throws Exception {
+        // arrange
+        var requestBody = new RegisterDTO("newUser", "test", "Name Test", "email@test.com");
+        when(repository.save(any())).thenReturn(userMock);
+
+        // act
+        var result = mvc.perform(
+                post("/auth/admin/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(registerDTOJson.write(requestBody).getJson()))
+                .andReturn().getResponse();
+
+        // assert
+        Assertions.assertEquals(HttpStatus.FORBIDDEN.value(), result.getStatus(), "The status should be 403 FORBIDDEN");
+    }
+}
---
+++ src/test/java/br/com/blogsanapi/utils/TokenUtils.java
@@ -0,0 +1,22 @@
+package br.com.blogsanapi.utils;
+
+import java.util.Base64;
+
+public class TokenUtils {
+
+    	public static boolean isValidTokenFormat(String token) {
+		String[] parts = token.split("\\.");
+
+		if(parts.length != 3) return false;
+
+		try {
+			Base64.getUrlDecoder().decode(parts[0]);
+			Base64.getUrlDecoder().decode(parts[1]);
+			Base64.getUrlDecoder().decode(parts[2]);
+			return true; 
+
+		} catch (IllegalArgumentException ex) {
+			return false;
+		}
+	}
+}
