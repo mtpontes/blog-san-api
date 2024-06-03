a120fff5cae53935b90b3fd8027514b61bb2f16d
Mateus Pontes <mateuspsdd@gmail.com>
2024-06-03 14:14:27 -0300

    Add `H2Test` and `MySQLTestContainer` classes

--- src/test/java/br/com/blogsanapi/integration/MySQLTestContainer.java
+++ src/test/java/br/com/blogsanapi/integration/MySQLTestContainer.java
@@ -2,31 +2,26 @@ package br.com.blogsanapi.integration;
 
 import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
-import org.testcontainers.containers.MySQLContainer;
-import org.testcontainers.utility.DockerImageName;
+import org.testcontainers.containers.GenericContainer;
 
 public class MySQLTestContainer implements BeforeAllCallback {
 
-    private MySQLContainer<?> mysqlContainer;
-    
+    private static GenericContainer<?> mysql;
+
     @Override
     public void beforeAll(ExtensionContext context) throws Exception {
-        this.criaContainer();
-        this.configProperties();
-    }
+            mysql = new GenericContainer("mysql:8.0.36")
+                .withExposedPorts(3306)
+                .withEnv("MYSQL_ROOT_PASSWORD", "root");
+            mysql.start();
 
-    private void configProperties() {
-        System.setProperty("spring.datasource.url", mysqlContainer.getJdbcUrl());
-        System.setProperty("spring.datasource.username", mysqlContainer.getUsername());
-        System.setProperty("spring.datasource.password", mysqlContainer.getPassword());
-        System.setProperty("spring.datasource.driver-class-name", mysqlContainer.getDriverClassName());
-    }
-    
-    private void criaContainer() {
-        this.mysqlContainer = new MySQLContainer<>(DockerImageName.parse("mysql:8.0.36"))
-                .withDatabaseName("test")
-                .withUsername("root")
-                .withPassword("root");
-        this.mysqlContainer.start();
+            Integer mappedPort = mysql.getMappedPort(3306);
+            System.setProperty("spring.datasource.driver-class-name", "com.mysql.cj.jdbc.Driver");
+            System.setProperty("spring.datasource.url", "jdbc:mysql://localhost:" + mappedPort + "/" + context.getDisplayName() + "?createDatabaseIfNotExist=true");
+            System.setProperty("spring.datasource.username", "root");
+            System.setProperty("spring.datasource.password", "root");
+            
+            System.setProperty("spring.jpa.show-sql", "false");
+            System.setProperty("spring.jpa.hibernate.ddl-auto", "update");
     }
 }
