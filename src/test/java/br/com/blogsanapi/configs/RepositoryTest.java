2a4120b13953fffed845b7f9519dd4758b5abd9d
Mateus Pontes <mateuspsdd@gmail.com>
2024-06-08 15:33:24 -0300

    feat: add `RepositoryTest` class

---
+++ src/test/java/br/com/blogsanapi/configs/RepositoryTest.java
@@ -0,0 +1,18 @@
+package br.com.blogsanapi.configs;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Import(MySQLTestContainerConfigs.class)
+public @interface RepositoryTest {}
