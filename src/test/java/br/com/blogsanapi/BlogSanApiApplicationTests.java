9fc8a89aa06d4866a1eee0d65040d161726082e8
Mateus Pontes <mateuspsdd@gmail.com>
2024-06-28 16:53:50 -0300

    test: fix: fixes database connection error in `BlogSanApiApplicationTests`

--- src/test/java/br/com/blogsanapi/BlogSanApiApplicationTests.java
+++ src/test/java/br/com/blogsanapi/BlogSanApiApplicationTests.java
@@ -1,10 +1,12 @@
 package br.com.blogsanapi;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@AutoConfigureTestDatabase
 @ActiveProfiles("test")
 class BlogSanApiApplicationTests {
 
